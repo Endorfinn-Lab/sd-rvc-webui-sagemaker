ca7ad818756c1d6953d4d42bab0977cd3ba568c8
Endorfinn <127268790+Endorfinn-Lab@users.noreply.github.com>
2024-08-05 15:48:46 +0700

    Delete gdrive_download.py

--- gdrive_download.py
+++
@@ -1,7 +0,0 @@
-import gdown
-
-file_id = '1xPbqLJYpa1UTHvm5kQe9GOSyAKczcsKz'  # Replace with the actual ID. How to find : https://drive.google.com/file/d/FILE_ID/view?usp=sharing
-output_filename = 'nagi_XL_v2.safetensors'  # Replace with desired filename
-
-gdown.download(id=file_id, output=output_filename)
-print(f"Downloaded file to {output_filename}")
