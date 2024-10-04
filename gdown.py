bf58f19f0b6770b9a0073d6aabcbada46ce19f08
Endorfinn <127268790+Endorfinn-Lab@users.noreply.github.com>
2024-10-04 14:28:00 +0700

    Update gdown.py

--- gdown.py
+++ gdown.py
@@ -8,7 +8,7 @@ destination = input("Destination Folde(Leave blank for current folder): ")
 if not destination:
   destination = "."
 
-filename = os.path.join(destination file_id)
+filename = os.path.join(destination, file_id)
 
 gdown.download(id=file_id, output=filename, quiet=False)
 print(f"Downloaded file {filename}")
